448f160338d4e50a246b072c85398e5f0c7f090d
crazypigeon <5056946+crazypigeon@users.noreply.github.com>
2017-10-29 17:51:03 -0700

    got basic caesar encryption working via command line arguments

--- crypto.py
+++ crypto.py
@@ -1,12 +1,27 @@
 import argparse
 
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-caesar = alpha[3:] + alpha[:3]
-
 parser = argparse.ArgumentParser(description='Test')
 
-parser.add_argument('-c',nargs='+',required=True,help='crypt some text',metavar='text to encrypt')
+parser.add_argument('-t',nargs='+',required=True,help='crypt some text',metavar='text to encrypt')
+parser.add_argument('-s',choices=range(0,25),default=3,type=int,help='# of characters to shift')
 
-crypt_text = ' '.join(parser.parse_args().c)
+plain_alpha = 'abcdefghijklmnopqrstuvwxyz'
+shift = parser.parse_args().s
+crypt_alpha = plain_alpha[shift:] + plain_alpha[:shift]
 
-print('"' + crypt_text + '"')
+plain_text = ' '.join(parser.parse_args().t)
+
+print plain_alpha
+
+print crypt_alpha
+
+
+print plain_text
+
+crypt_text = ''
+for l in plain_text:
+    try:
+        crypt_text += (crypt_alpha[plain_alpha.index(l)])
+    except ValueError:
+        pass
+print crypt_text

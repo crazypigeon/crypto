f7dd85d3d8c8444c0c24225a566ae5c9c392c434
crazypigeon <5056946+crazypigeon@users.noreply.github.com>
2017-10-29 18:10:45 -0700

    made caesar shift into a class

--- crypto.py
+++ crypto.py
@@ -5,23 +5,33 @@ parser = argparse.ArgumentParser(description='Test')
 parser.add_argument('-t',nargs='+',required=True,help='crypt some text',metavar='text to encrypt')
 parser.add_argument('-s',choices=range(0,25),default=3,type=int,help='# of characters to shift')
 
-plain_alpha = 'abcdefghijklmnopqrstuvwxyz'
-shift = parser.parse_args().s
-crypt_alpha = plain_alpha[shift:] + plain_alpha[:shift]
+class CaesarCrypt:
+    def __init__(self):
+        self.plain_alpha = 'abcdefghijklmnopqrstuvwxyz'
+        self.crypt_alpha = ''  
+        self.set_key()
 
-plain_text = ' '.join(parser.parse_args().t)
+    def set_key(self,shift=3):
+        if 0 <= shift <= 25:
+            self.shift = shift
+            self.crypt_alpha = self.plain_alpha[shift:] + self.plain_alpha[:shift]
+        else:
+            raise ValueError("Shift must be between 0 and 25")
 
-print plain_alpha
+    def encrypt(self,plain_text):
+        crypt_text = ''
+        for l in plain_text:
+            try:
+                crypt_text += (self.crypt_alpha[self.plain_alpha.index(l)])
+            except ValueError:
+                pass
+        return crypt_text
+        
 
-print crypt_alpha
+crypt = CaesarCrypt()
 
+crypt.set_key(parser.parse_args().s)
 
-print plain_text
-
-crypt_text = ''
-for l in plain_text:
-    try:
-        crypt_text += (crypt_alpha[plain_alpha.index(l)])
-    except ValueError:
-        pass
-print crypt_text
+user_text = ' '.join(parser.parse_args().t)
+print user_text
+print crypt.encrypt(user_text)
